651ef89129787e2eed6a8a5890f5e3fffa93c010
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro-2.local>
2019-03-22 11:34:09 -1000

    Setting Up Layout

---
+++ src/containers/Homepage/Homepage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Homepage.css';
+
+class Homepage extends Component {
+    render() {
+        return (
+            <div className='Homepage'>
+                HOMEPAGE
+            </div>
+        )
+    }
+}
+
+export default Homepage;
